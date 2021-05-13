b370df9b75a19683ec8cff7c96b8d989948efe3c
real name genij <zgxzgx@gmail.com>
2021-05-12 22:07:05 -0500

    feat: add comic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { Table } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        Table of Results<br/>
+        <Table>
+          <thead>
+            <tr>
+              <th>Input: </th>
+              <th>Prime Factorization</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>8</td>
+              <td>...</td>
+              <td>
+                <button color="success" size="sm">Prime Factorize!</button>
+                <button color="danger" size="sm">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+        <img src="src/xkcd247.png" alt="xkcd comic 247"/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
